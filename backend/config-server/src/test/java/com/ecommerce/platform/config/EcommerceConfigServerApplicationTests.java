55786a0280c9b11880fd10e9c260aaf90ebd650c
ashishrajput35 <ashishrajput35cs@gmail.com>
2025-04-11 00:13:52 +0530

    resolve error in test class

--- backend/config-server/src/test/java/com/ecommerce/platform/config/EcommerceConfigServerApplicationTests.java
+++ backend/config-server/src/test/java/com/ecommerce/platform/config/EcommerceConfigServerApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {
 		"spring.cloud.config.server.git.uri=https://github.com/ashishrajput35/ecommerce-config-repo.git",
-		"spring.cloud.config.server.git.default-label=develop"
+		"spring.cloud.config.server.git.default-label=dev"
 })
 class EcommerceConfigServerApplicationTests {
 
